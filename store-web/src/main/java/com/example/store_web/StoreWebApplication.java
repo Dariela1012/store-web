085a9157826e6f38b96f18d9604ce7ab399e5d58
Karen Dariela Abarca Jaramillo <karendariela12@gmail.com>
2025-09-24 10:37:42 -0500

    Primer commit: proyecto store-web

---
+++ store-web/src/main/java/com/example/store_web/StoreWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.store_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreWebApplication.class, args);
+	}
+
+}
