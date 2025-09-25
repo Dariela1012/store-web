096f89babf44fe154037c50b477f68819f249e6a
Karen Dariela Abarca Jaramillo <karendariela12@gmail.com>
2025-09-25 12:38:33 -0500

    cuarto commit: creación clase AdminUsuario.java

---
+++ store-web/src/main/java/com/example/store_web/model/AdminUsuario.java
@@ -0,0 +1,14 @@
+package com.example.store_web.model;
+
+import jakarta.persistence.*;
+import java.util.Collection;
+import java.util.Collections;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+@Table(name = "admin_usuario")
+public class AdminUsuario {
+    
+}
