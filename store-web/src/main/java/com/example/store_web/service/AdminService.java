7af20d4d9a5d9f1220ad8498287e8e6184c236e2
Karen Dariela Abarca Jaramillo <karendariela12@gmail.com>
2025-09-26 13:17:39 -0500

    código AdminService.java terminado

--- store-web/src/main/java/com/example/store_web/service/AdminService.java
+++ store-web/src/main/java/com/example/store_web/service/AdminService.java
@@ -8,5 +8,17 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AdminService{
-    
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public AdminUsuario buscarPorUsuario(String usuario){
+        try{
+            return entityManager.createQuery("SELECT a FROM AdminUsuario a WHERE a.usuario = :usuario", AdminUsuario.class)
+                    .setParameter("Usuario", usuario)
+                    .getSingleResult();    
+        } catch(NoResultException e){
+            return null;
+        }
+    }
 }
