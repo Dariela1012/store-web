7a022a63bef5db456c5535b4ff6c78b441de69bd
Karen Dariela Abarca Jaramillo <karendariela12@gmail.com>
2025-09-26 13:11:42 -0500

    creación de la clase AdminService.java

---
+++ store-web/src/main/java/com/example/store_web/service/AdminService.java
@@ -0,0 +1,12 @@
+package com.example.store_web.service;
+
+import com.example.store_web.model.AdminUsuario;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminService{
+    
+}
