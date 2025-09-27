24156a32142c0f485e28d22187b9734ed89ef463
Karen Dariela Abarca Jaramillo <karendariela12@gmail.com>
2025-09-27 10:28:33 -0500

    código en Producto.java

--- store-web/src/main/java/com/example/store_web/model/Producto.java
+++ store-web/src/main/java/com/example/store_web/model/Producto.java
@@ -1,3 +1,14 @@
+package com.example.store_web.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "producto")
 public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private Integer id_producto;
     
 }
