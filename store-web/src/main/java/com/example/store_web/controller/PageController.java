964be04f66f355ed8c7f973d35d9796b1cc37045
Karen Dariela Abarca Jaramillo <karendariela12@gmail.com>
2025-09-30 17:36:06 -0500

    código en PageController.java

--- store-web/src/main/java/com/example/store_web/controller/PageController.java
+++ store-web/src/main/java/com/example/store_web/controller/PageController.java
@@ -1,5 +1,23 @@
 package com.example.store_web.controller;
 
+import com.example.store_web.model.Categoria;
+import com.example.store_web.model.Contacto;
+import com.example.store_web.model.Producto;
+import com.example.store_web.service.CategoriaService;
+import com.example.store_web.service.ContactoService;
+import com.example.store_web.service.EmailService;
+import com.example.store_web.service.ProductoService;
+import jakarta.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class PageController{
     
 }
