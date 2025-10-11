73d18f6232a653389c2e69e1359d081db5497b11
Karen Dariela Abarca Jaramillo <karendariela12@gmail.com>
2025-10-11 11:50:48 -0500

    Creación de la clase ProductoService.java

---
+++ store-web/src/main/java/com/example/store_web/service/ProductoService.java
@@ -0,0 +1,15 @@
+package com.example.store_web.service;
+
+import com.example.store_web.model.Producto;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.ParameterMode;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.StoredProcedureQuery;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductoService {
+}
