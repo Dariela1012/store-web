23c4792eaeaed1ac6c9131db57750149735e1c31
Karen Dariela Abarca Jaramillo <karendariela12@gmail.com>
2025-10-12 10:18:06 -0500

    Código en ProductoService.java

--- store-web/src/main/java/com/example/store_web/service/ProductoService.java
+++ store-web/src/main/java/com/example/store_web/service/ProductoService.java
@@ -12,4 +12,32 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ProductoService {
+
+  @PersistenceContext
+  private EntityManager entityManager;
+
+  @Transactional
+  public void guardar(Producto producto) {
+    entityManager.persist(producto);
+  }
+
+  @Transactional
+  public String actualizar(Producto productoNuevo, int id) {
+    String message = "mensaje";
+    Producto producto = entityManager.find(Producto.class, id);
+    // Actualizar datos
+    producto.setNombre(productoNuevo.getNombre());
+    producto.setDescripcion(productoNuevo.getDescripcion());
+    producto.setPrecio(productoNuevo.getPrecio());
+    producto.setImagenUrl(productoNuevo.getImagenUrl());
+    producto.setTalla(productoNuevo.getTalla());
+    producto.setGenero(productoNuevo.getGenero());
+    producto.setEdadSugerida(productoNuevo.getEdadSugerida());
+    producto.setCategoria(productoNuevo.getCategoria());
+    entityManager.merge(producto);
+
+    message = "¡Producto actualizado correctamente!";
+    return message;
+   }
+
 }
