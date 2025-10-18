a641ce64e5e856dc36b5833f19781aea74c0b7d1
Karen Dariela Abarca Jaramillo <karendariela12@gmail.com>
2025-10-18 12:27:35 -0500

    código

--- store-web/src/main/java/com/example/store_web/service/ContactoService.java
+++ store-web/src/main/java/com/example/store_web/service/ContactoService.java
@@ -1,12 +1,10 @@
 package com.example.store_web.service;
 
-import org.springframework.stereotype.Service;
-
-@Service
 public class ContactoService {
 
-    public String enviarMensaje() {
-        return "Mensaje enviado desde ContactoService (versión 1)";
-    }
-}
+  private EntityManager entityManager;
 
+  public void guardar(Contacto contacto) {
+    entityManager.persist(contacto);
+  }
+}
