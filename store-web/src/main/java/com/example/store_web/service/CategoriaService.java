f882022a0986b6f8a15d3f99d0b0f1b0f98112e5
Karen Dariela Abarca Jaramillo <karendariela12@gmail.com>
2025-10-18 12:38:31 -0500

    código corregido en CategoriaService.java

--- store-web/src/main/java/com/example/store_web/service/CategoriaService.java
+++ store-web/src/main/java/com/example/store_web/service/CategoriaService.java
@@ -1,10 +1,17 @@
 package com.example.store_web.service;
 
+import com.example.store_web.model.Categoria;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CategoriaService {
-    public String listarCategoria(){
-        return "lista de categorías";
-    }
+  @PersistenceContext
+  private EntityManager entityManager;
+  
+  public List<Categoria> listarTodos() {
+    return entityManager.createQuery("SELECT c FROM Categoria c", Categoria.class).getResultList();
+  }
 }
