02bdf277ddfb1b31c5800a9892a798ed9228af97
Karen Dariela Abarca Jaramillo <karendariela12@gmail.com>
2025-10-24 10:58:10 -0500

    revisión en StoreWebApplication.java

--- store-web/src/main/java/com/example/store_web/StoreWebApplication.java
+++ store-web/src/main/java/com/example/store_web/StoreWebApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StoreWebApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(StoreWebApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(StoreWebApplication.class, args);
+  }
 }
